058dba702f5e7e5fd4c4781ff15f69c545cb1c93
Zehan Khan <zehan9211@gmail.com>
2021-12-25 14:48:05 +0530

    added submit

--- block-BJaair/code/script.js
+++ block-BJaair/code/script.js
@@ -1,5 +1,9 @@
 let form = document.querySelector("form");
 let div = document.querySelector(".div");
+div.style.heigth = "100vh";
+div.style.display = "flex";
+div.style.flexWrap = "wrap";
+
 
 let userInfo = {};
 
@@ -9,10 +13,16 @@ function handelSubmit(event){
     userInfo.option = form.elements.option.value;
     console.log(userInfo);
     let box = document.createElement("div")
+    box.style.padding = "50px 70px";
     // box.classList.add();
-    let input = document.createElement('input');
-    input.setAttribute('type', 'text');
-    
+    // let input = document.createElement('input');
+    // input.setAttribute('type', 'text');
+    let h2 = document.createElement("h2");
+    h2.innerText = userInfo.text;
+    let p = document.createElement("p");
+    p.innerText = userInfo.option;
+    box.append(h2,p);
+    div.prepend(box)
 }
 
 form.addEventListener('submit',handelSubmit);
