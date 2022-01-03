7d36a858a6128743d6a3254345b45ec514b323f5
Zehan Khan <zehan9211@gmail.com>
2022-01-03 20:10:53 +0530

    test 1

--- block-BJaajc/code/index.test.js
+++ block-BJaajc/code/index.test.js
@@ -1,1 +1,22 @@
-const index = require
+const index = require('./index');
+
+test('getFullNames "Zehan" + "Khan" to equal "Zehan Khan"', ()=>{
+    expect(getFullName("Zehan","Khan")).toBe("Zehan Khan");
+});
+
+test("add 1 + 2 to equal 3" , ()=> {
+    expect(add(1,2)).toBe(3);
+});
+
+test("add 100 + 2 to equal 102" , ()=> {
+    expect(add(100,2)).toBe(102);
+});
+
+
+test("add 10000 + 2 to equal 10002" , ()=> {
+    expect(add(10000,2)).toBe(10002);
+});
+
+test("add 4 + 2 to not equal 6" , ()=> {
+    expect(add(4,2)).not.toBe(6);
+});
